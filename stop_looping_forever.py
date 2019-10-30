a4a7b8c4f11374d0313929d5c58b2aa395e2df27
zeus <33592162+1nam@users.noreply.github.com>
2019-10-30 07:53:00 -0400

    Create stop_looping_forever.py

---
+++ stop_looping_forever.py
@@ -0,0 +1,6 @@
+# loop
+x = 2          #x is now 2
+while x <= 10:  # 2 is less than 10
+    print(x)     # print 
+    x += 1        # if this were used  x = 2 + 1 Loops forever.
+print("using short code x+= 1 this fixes the loop forever issue")
